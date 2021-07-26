998b5ff415a7aef2acb6774f629e424fa306477a
zhi <1106010597@qq.com>
2021-07-26 15:08:13 +0800

    重复三次布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    Money.vue
+  <div class="nav-wrapper">
+    <div class="content">Money.vue</div>
     <Nav />
   </div>
 </template>
@@ -12,5 +12,15 @@
 </script>
 
 <style scoped>
-
+.nav-wrapper{
+  border: 1px solid green;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+  .content{
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    Statistics.vue
-    <Nav />
+  <div class="nav-wrapper">
+    <div class="content">Statistics.vue</div>
+
+    <Nav/>
   </div>
 </template>
 
@@ -12,5 +13,16 @@
 </script>
 
 <style scoped>
+  .nav-wrapper {
+    border: 1px solid green;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
 
+  .content {
+    border: 1px solid blue;
+    overflow: auto;
+    flex-grow: 1;
+  }
 </style>
