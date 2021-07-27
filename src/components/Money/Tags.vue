1ee48a3dd106f325010133bec44f5fa16fc5a9e8
zhi <1106010597@qq.com>
2021-07-27 18:19:52 +0800

    完成 tags 组件

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,22 +1,48 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button>新增标签</button>
+      <button @click="create">新增标签</button>
     </div>
     <ul class="current">
-      <li>衣</li>
-      <li>食</li>
-      <li>住</li>
-      <li>行</li>
+      <li v-for="tag in dataSource" :key="tag"
+      @click="toggle(tag)"
+      :class="{selected: selectedTags.indexOf(tag) >= 0}">{{tag}}</li>
     </ul>
   </div>
 </template>
 
 <script lang="ts">
-  import {defineComponent} from 'vue';
-
+  import {defineComponent } from 'vue';
   export default defineComponent({
-
+    props:{
+      dataSource:  [String]
+    },
+    emits: ['update:dataSource'],
+    data(){
+      return{
+        selectedTags: [] as string[]
+      }
+    },
+    methods:{
+      toggle(tag: string){
+        const index = this.selectedTags.indexOf(tag)
+        if(index >= 0){
+          this.selectedTags.splice(index, 1)
+        }else {
+          this.selectedTags.push(tag)
+        }
+      },
+      create(){
+        const name = window.prompt('请输入标签名')
+        if(name === ''){
+          window.alert('标签名不能为空')
+        }else {
+          if(this.dataSource){
+            this.$emit('update:dataSource',[...this.dataSource, name])
+          }
+        }
+      }
+    }
   });
 </script>
 
@@ -39,6 +65,10 @@
         padding: 0 16px;
         margin-right: 12px;
         margin-top: 4px;
+        &.selected{
+          background: #F0C48A;
+          color: white;
+        }
       }
     }
     > .new{
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad />
     <Types/>
     <Notes/>
-    <Tags />
+    <Tags :data-source="tags" v-model:dataSource="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
   import Layout from '@/components/Layout.vue';
   import Tags from '@/components/Money/Tags.vue';
   import NumberPad from '@/components/Money/NumberPad.vue';
@@ -15,6 +15,12 @@
   import Notes from '@/components/Money/Notes.vue';
 
   export default {
+    // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+    data(){
+      return{
+        tags: ['衣','食','住','行']
+      }
+    },
     name: 'Money',
     components: {Notes, Types, NumberPad, Tags, Layout},
   }
