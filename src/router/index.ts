5abc01b49f1c567cd7f475ae5d7c4649df78863a
zhi <1106010597@qq.com>
2021-07-28 18:36:03 +0800

    将 tag 的 string 改为 {id， name}

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,7 @@ import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue'
+import EditLabel from '@/views/EditLabel.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -20,6 +21,10 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/statistics',
     component: Statistics
+  },
+  {
+    path: '/labels/edit',
+    component: EditLabel
   },
   {
     path: '/:catchAll(.*)',
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    编辑标签
+  </Layout>
+</template>
+
+<script lang="ts">
+  import {defineComponent} from 'vue';
+  import Layout from '@/components/Layout.vue';
+
+  export default defineComponent({
+    components: {Layout}
+  });
+</script>
+
+<style lang="scss" scoped>
+
+</style>
